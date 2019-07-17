3ad038ff24ccc50f5d1d084622bb998bb19447bb
Tarepan <tarepan5884@gmail.com>
2019-07-18 07:58:13 +0900

    Add type-checking with Pyright

--- tempy/main.py
+++ tempy/main.py
@@ -1,1 +1,15 @@
 print("This is tempy!!")
+
+# Type-checking OK
+def please_typecheck1(arg1: int) -> int:
+    return 1
+
+# Pyright show warning toward int-string missmatch
+def please_typecheck2(arg1: int) -> int:
+    return "a"
+
+# Type-checking OK
+please_typecheck1(1)
+
+# Pyright show warning toward int-string missmatch
+please_typecheck1("a")
